3a79ee0acf9bce17528381bffb8c213eec27ad94
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2018-11-23 14:44:15 -0600

    Created a function "getID" that will get the steamID of a person as long as they have a vanity URL. The steamID is needed in order to get match data from the dota2api

--- bot.py
+++ bot.py
@@ -11,8 +11,29 @@ NICK = 'Nubot'
 IDENT = 'Nubot'
 CHANNEL = '#dotanoobs'
 
+
 s = socket.socket()
 API = dota2api.Initialise('***REMOVED***')
 DOTABUFF = re.compile(":http://www.dotabuff.com/matches\d+")
-match_id = '4173686982'
-print(API.get_match_details(match_id=match_id))
+
+
+def getID(name):
+    """This function gets the steam ID of a person as long as they
+have a vanity URL. The steam ID is needed in order to use the dota2api"""
+
+    import requests
+    IDAPI = ('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/'
+             '?key=***REMOVED***&vanityurl='
+             + name
+             )
+
+    response = requests.get(IDAPI)
+    data = response.content
+    d2 = json.loads(data)
+    id = d2['response']['steamid']
+    print('Steam ID:' + id)
+
+    return id
+
+
+getID('binaryatrocity')
