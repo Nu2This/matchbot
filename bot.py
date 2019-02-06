9e8e0ceabe0d4a25e9bdcf4273d1d525b03c2094
Michael Heidelberger <heidelberger.michael@gmail.com>
2019-02-06 04:30:28 +0000

    First fully working version of the bot. Can take !matches and !register from IRC chat and returns correctly.

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import argparse
 import dota2api
 import requests
 import json
@@ -11,14 +12,84 @@ HOST = 'chat.freenode.net'
 PORT = '6667'
 NICK = 'Nubot'
 IDENT = 'Nubot'
-CHANNEL = '#dotanoobs'
+CHANNEL = '##nubottest'
+ADMINNAME = 'Nu2This'
+EXITCODE = "bye " + NICK
 
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s = socket.socket()
 APIKEY = '***REMOVED***'
 API = dota2api.Initialise(APIKEY)
 DOTABUFF = re.compile(":http://www.dotabuff.com/matches\d+")
 
+# Here we connect to the server using the port 6667
+ircsock.connect((HOST, 6667))
+# We are basically filling out a form with this line and saying to set all 
+# the fields to the bot nickname.
+ircsock.send(bytes("USER "+ NICK +" "+ NICK +" "+ NICK + " "
+                   + NICK + "\n", "UTF-8"))
+# assign the nick to the bot
+ircsock.send(bytes("NICK "+ NICK +"\n", "UTF-8"))
 
-#getstuff.getID(APIKEY, 'Nu2This')
-getstuff.getMatches(APIKEY, 'UnoPolak')
+def joinchan(chan): # join channel(s).
+    ircsock.send(bytes("JOIN "+ CHANNEL +"\n", "UTF-8"))
+    ircmsg = ""
+    while ircmsg.find("End of /NAMES list.") == -1:
+        ircmsg = ircsock.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('\n\r')
+        print(ircmsg)
+
+
+def ping(): # respond to server Pings.
+    ircsock.send(bytes("PONG :pingis\n", "UTF-8"))
+    print('pong')
+
+
+def sendmsg(msg, target=CHANNEL): # sends messages to the target.
+    ircsock.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
+
+
+if __name__ == '__main__':
+    # Join the CHANNEL
+    joinchan(CHANNEL)
+    # Run the bot
+    while True:
+        # These lines listen to the channel
+        ircmsg = ''
+        ircmsg = ircsock.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('\n\r')
+        print(ircmsg)
+        # Get the name from the line and Check to make sure it isnt a private
+        # message
+        if ircmsg.find("PRIVMSG") != -1:
+            name = ircmsg.split('!',1)[0][1:]
+            message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+            print('name: ' + name)
+        if ircmsg.find('PING :'):
+            ping()
+        if message[:6] == 'PING :':
+            ping()
+        if message[:8] == '!matches':
+            if getstuff.isRegisterd(name) == False:
+                sendmsg('ID not found please !register')
+            else:
+                toSend = getstuff.getMatches(APIKEY, name, 5)
+                for item in toSend:
+                    sendmsg(item)
+        if message[:9] == '!register':
+            getstuff.getID(APIKEY, name)
+            sendmsg('Registrashion Compreeto!')
+
+        if message[:5] == '!quit':
+            ircsock.send(bytes("QUIT \n", "UTF-8"))
+
+
+   # parser = argparse.ArgumentParser()
+   # parser.add_argument('name', type=str)
+   # parser.add_argument('-register', action='store_true')
+
+   # args = parser.parse_args()
+   # if args.register:
+   #     getstuff.getID(APIKEY, args.name)
+   # else:
+   #     getstuff.getMatches(APIKEY, args.name)
--- getstuff.py
+++ getstuff.py
@@ -1,6 +1,8 @@
+import pudb
 import dota2api
 import requests
 import json
+import sys
 
 
 
@@ -13,22 +15,76 @@ def getID(APIKEY, name):
     response = requests.get(IDAPI)
     d2 = json.loads(response.content.decode('utf-8'))
     id = d2['response']['steamid']
-    print('Steam ID:' + id)
+    print('Steam ID:' + str(id))
+    id32 = int(id) - 76561197960265728
+    print(id32)
+    print(id)
     with open('id.txt', 'a') as f:
-        f.write(name + ' ' + id + '\n')
+        f.write(name + ' ' + str(id32) +  ' ' + id + '\n')
 
 
-def getMatches(APIKEY, name):
-    API = dota2api.Initialise(APIKEY)
+def isRegisterd(name):
     with open('id.txt', 'r') as f:
         # If the user is in the id file get his steamID number
         found = False
         for line in f:
             if name in line:
                 found = True
-                id = line.split()[1]
-                print(line.split()[1])
+                id = line.split()[2]
+                return id
+        # If the user is not in the id file get it
         if found == False:
-            print('ID not Found Searching...')
-            getID(APIKEY, name)
-    print(API.get_match_history(account_id=id))
+            print('ID not Found please !register...')
+            return False
+
+def getMatches(APIKEY, name, x):
+    API = dota2api.Initialise(APIKEY)
+    sendBack = []
+    STEAMID64 = isRegisterd(name)
+    # For some reason the dota2api gets the 32 bit steam Ids so we convert
+    # the 64 bit one.
+    STEAMID32 = int(STEAMID64) - 76561197960265728
+    # dota2api get_match_history params
+        # account_id (int, optional)
+        # hero_id (int, optional)
+        # game_mode (int, optional)
+        # skill (int, optional)
+        # min_players (int, optional)
+        # league_id (int, optional)
+        # start_at_match_id (int, optional)
+        # matches_requested (int, optional)
+        # tournament_games_only (str, optional)
+    dotadata = API.get_match_history(account_id=STEAMID64, matches_requested=x)
+    for item in dotadata['matches']:
+        matchDetails = API.get_match_details(item['match_id'])
+        for player in matchDetails['players']:
+            if player['account_id'] == STEAMID32:
+                pTeam = False
+                winlose = 'Loss'
+                radire = 'Dire'
+                if player['player_slot'] < 5:
+                    pTeam = True
+                    radire = 'Radiant'
+                if pTeam == matchDetails['radiant_win']:
+                    winlose = 'Win!'
+                sendBack.append(name + ' ' + winlose + ' ' + radire +
+                                " {} {} K/D/A:{}/{}/{} "
+                                "LH/D:{}/{} XPM:{} "
+                                "GPM:{}".format(player['hero_name'],
+                                                player['level'],
+                                                player['kills'],
+                                                player['deaths'],
+                                                player['assists'],
+                                                player['last_hits'],
+                                                player['denies'],
+                                                player['xp_per_min'],
+                                                player['gold_per_min']))
+    return sendBack
+
+
+
+
+
+
+
+
